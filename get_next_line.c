28421ead33bd9b20a4ab21b00ab077bca6c90ac9
Iñigo <inigo@MacBook-Pro-de-Mikel.local>
2022-11-08 20:57:54 +0100

    0.1st version after understing the subject properly

--- get_next_line.c
+++ get_next_line.c
@@ -3,50 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iblanco- <iblanco-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: inigo <inigo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 18:16:02 by inigo             #+#    #+#             */
-/*   Updated: 2022/11/07 18:21:58 by iblanco-         ###   ########.fr       */
+/*   Updated: 2022/11/08 20:31:45 by inigo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 char	*get_next_line(int fd)
 {
-	static ssize_t		i;
+	//static ssize_t		i;
 	static char			*acumulator;
 	char				buffer[BUFFER_SIZE];
-	char				ret[1];
+	char				*temp;
 
-	i = 0;
+
+	//i = 0;
 	if (fd < -1)
 		return (NULL);
-	read(fd, buffer, BUFFER_SIZE);
-	while (buffer[i++] != '\n')
-		i++;
-	ft_memcpy(&ret[i], &buffer[i], 1);
-	if (ret[i] == '\n')
-	{	
-		break ;
-		i++;
+	acumulator = malloc(1);
+	if (!acumulator)
+		return (NULL);
+	acumulator[0] = '\0';
+	//read(fd, buffer, BUFFER_SIZE);
+	while (BUFFER_SIZE == read(fd, buffer, BUFFER_SIZE))
+	{
+		temp = ft_strjoin(acumulator, buffer);
+		acumulator = temp;
+		//printf("\nDEPURAR %s", acumulator);
 	}
-	return (ret);
+	return (acumulator);
 }
 
-// int main()
-// {
-// 	int fd1 = open("test.txt", O_RDONLY);
-// 	if (fd1 < 0)
-// 	{
-// 		perror("c1");
-// 		exit(1);
-// 	}
-// 	printf("\nreturned string is = %s", get_next_line(fd1));
-// 	if (close(fd1) < 0)
-// 	{
-// 		perror("c1");
-// 		exit(1);
-// 	}
-// 	//printf("\nclosed the fd.\n");
-// }
+int main()
+{
+	int fd1 = open("test.txt", O_RDONLY);
+	if (fd1 < 0)
+	{
+		perror("c1");
+		exit(1);
+	}
+	printf("\nreturned string is = \n%s", get_next_line(fd1));
+	if (close(fd1) < 0)
+	{
+		perror("c1");
+		exit(1);
+	}
+	//printf("\nclosed the fd.\n");
+}
