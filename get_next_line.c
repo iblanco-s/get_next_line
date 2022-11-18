503e14db253f6ba1d32dfdeaf80d50432ed94f56
iblanco-s <iblanco-@student.42urduliz.com>
2022-11-18 19:12:34 +0100

    from 42

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: inigo <inigo@student.42.fr>                +#+  +:+       +#+        */
+/*   By: iblanco- <iblanco-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 18:16:02 by inigo             #+#    #+#             */
-/*   Updated: 2022/11/16 19:48:14 by inigo            ###   ########.fr       */
+/*   Updated: 2022/11/18 19:08:45 by iblanco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,40 +18,54 @@ char	*get_next_line(int fd)
 	static int			i = -2;
 	static int			j = BUFFER_SIZE;
 	static char			*acumulator;
-	static char			*temp;
-	char				buffer[BUFFER_SIZE];
+	char				*temp;
+	char				*buffer;
 
-	if (fd < -1) // no seria menor que 0?
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	if (i == -2)
 	{
 		acumulator = malloc(1);
 		if (!acumulator)
 			return (NULL);
-		acumulator[0] = '\0'; 
+		acumulator[0] = '\0';
 	}
-    while (j == BUFFER_SIZE || ft_check_n(acumulator) >= 0)
-    {
-    	i = ft_check_n(acumulator); 
-    	if (i >= 0)
-    	{
+	while (j == BUFFER_SIZE || ft_check_n(acumulator) >= 0 || i == -2)
+	{
+		i = ft_check_n(acumulator);
+		if (i >= 0)
+		{
 			temp = acumulator;
-     		acumulator = ft_substr(temp, i + 1, ft_strlen(temp));
-			printf("\nDEBUGGER-ACUMULATOR = %s", acumulator);
-      		temp[i + 1] = '\0';
-      		return (temp);
-    	}
+			acumulator = ft_substr(temp, i + 1, ft_strlen(temp));
+			free (temp);
+			temp[i + 1] = '\0';
+			return (temp);
+		}
+		buffer = malloc(BUFFER_SIZE + 1);
+		if (!buffer)
+			return (NULL);
 		j = read(fd, buffer, BUFFER_SIZE);
+		buffer[j] = '\0';
 		temp = ft_strjoin(acumulator, buffer, j);
-    	acumulator = temp;
+		free (acumulator);
+		free (buffer);
+		acumulator = temp;
+		i = ft_check_n(acumulator);
 	}
-	//printf("\nDEBUGGER-BUFFER = %s", temp);
-	//acumulator = ft_mod_substr(temp, 0, ft_strlen(temp) + j);
+	if (i == -3)
+	{
+		free (acumulator);
+		return (NULL);
+	}
+	i = -3;
 	return (acumulator);
 }
 
+// //printf("\nDEBUGGER-ACUMULATOR = %s", acumulator);
+
 // int main(void)
 // {
+// 	char	*test;
 // 	int fd1 = open("test.txt", O_RDONLY);
 // 	if (fd1 < 0)
 // 	{
@@ -59,11 +73,16 @@ char	*get_next_line(int fd)
 // 		exit(1);
 // 	}
 // 	for(int i = 0; i < 4; i++)
-// 		printf("\nreturned string is = %s", get_next_line(fd1));
+// 	{
+// 		test = get_next_line(fd1);
+// 		printf("\nreturned string is = %s", test);
+// 	}
 // 	if (close(fd1) < 0)
 // 	{
+// 		printf("ha entrado");
 // 		perror("c1");
 // 		exit(1);
 // 	}
+// 	free (test);
 // 	//printf("\nclosed the fd.\n");
 // }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: inigo <inigo@student.42.fr>                +#+  +:+       +#+        */
+/*   By: iblanco- <iblanco-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 18:19:38 by inigo             #+#    #+#             */
-/*   Updated: 2022/11/16 19:28:20 by inigo            ###   ########.fr       */
+/*   Updated: 2022/11/18 13:14:18 by iblanco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,36 +22,15 @@ int	ft_strlen(const char *a)
 	return (len);
 }
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	char	*kdest;
-	char	*ksrc;
-
-	kdest = (char *)dest;
-	ksrc = (char *)src;
-	if (dest == NULL && src == NULL)
-		return (0);
-	while (n > 0)
-	{
-		*kdest = *ksrc;
-		kdest++;
-		ksrc++;
-		n--;
-	}
-	return (dest);
-}
-
 char	*ft_strjoin(char const *s1, char const *s2, int lens2)
 {
 	size_t		i;
-	size_t		j;
 	size_t		k;
 	char		*ret;
 
 	i = ft_strlen(s1);
-	j = lens2;
 	k = 0;
-	ret = (malloc((i + j + 1) * sizeof(char)));
+	ret = (malloc((i + lens2 + 1) * sizeof(char)));
 	if (ret == NULL)
 		return (0);
 	i = 0;
@@ -60,41 +39,15 @@ char	*ft_strjoin(char const *s1, char const *s2, int lens2)
 		ret[k] = s1[k];
 		k++;
 	}
-	while (j > 0)
+	while (lens2 > 0)
 	{
 		ret[k] = s2[i];
 		k++;
 		i++;
-		j--;
+		lens2--;
 	}
 	ret[k] = '\0';
 	return (ret);
-}
-
-size_t	ft_strlcpy(char *dest, const char *src, size_t len)
-{
-	size_t	i;
-	size_t	k;
-
-	i = 0;
-	k = 0;
-	while (src[i] != '\0')
-		i++;
-	if (len > 0)
-	{
-		while (src[k] != '\0')
-		{
-			dest[k] = src[k];
-			k++;
-			if (k == len)
-			{
-				k--;
-				break ;
-			}
-		}
-		dest[k] = '\0';
-	}
-	return (i);
 }
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
@@ -121,44 +74,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (ret);
 }
 
-char	*ft_mod_substr(char const *s, unsigned int start)
+int	ft_check_n(char *acumulator)
 {
-	char				*ret;
-	size_t				i;
-	size_t				len;
-	
-	len = ft_strlen(s);
+	int	i;
+
 	i = 0;
-	if (!s)
-		return (0);
-	else if ((unsigned int)ft_strlen(s) < start)
-		len = 0;
-	else if (ft_strlen(s) - start < len)
-		len = ft_strlen(s) - start;
-	ret = ((char *)malloc(sizeof(char) * (len + 1)));
-	if (ret == NULL)
-		return (0);
-	while (len > (size_t)i)
+	while (acumulator[i] != '\0')
 	{
-		ret[i] = s[start + i];
+		if (acumulator[i] == '\n')
+			return (i);
 		i++;
 	}
-	ret[i] = '\0';
-	return (ret);
+	if (acumulator[0] == '\0')
+		return (-3);
+	else
+		return (-1);
 }
-
-int ft_check_n(char *acumulator)
-{
-  int i;
- 
-  i = 0;
-  while (acumulator[i] != '\0')
-  {
-    if(acumulator[i] == '\n')
-      return (i);
-    i++;
-  }
-  i = -1;
-  return (i);
-}
-
